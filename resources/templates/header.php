0a4da14c64f02c6b79724254731fd8156ea882fa
Alex <alex@Alexs-MBP.picmonic.corp>
2015-06-30 11:54:05 -0700

    got rid of old stuff

--- resources/templates/header.php
+++
@@ -1,7 +0,0 @@
-<html>
-<head>
-<!--changes row background colors per logic-->
-<link rel="stylesheet" type="text/css" href="./resources/css/main.css">
-</head>
-<body>
-<h1>Joyent/Node commits list</h1>
