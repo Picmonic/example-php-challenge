541fde1646691446b99c58bea5fb07675a8b617b
cparrish <carl.parrish@gmail.com>
2015-07-29 23:39:45 +0000

    Create commits table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
       @foreach($issues as $issue)
       <div class="row">
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-          {{ $issue["number"]}}
+          <a href="{{$issue['html_url']}}">{{ $issue["number"]}}</a>
         </div>
         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
           {{$issue["title"]}}
