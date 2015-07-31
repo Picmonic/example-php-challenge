a2c258836e1639a5d638fa382b39204a4ad2b290
cparrish <carl.parrish@gmail.com>
2015-07-31 19:10:01 +0000

    Clean up repo for pull request

---
+++ source/app/Commit.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Commit extends Model
+{
+  protected $fillable = [
+    'number',
+    'userName',
+    'title',
+    'body',
+    'created_at',
+    'updated_at',
+  ];
+
+public function setCreatedAtAttribute($date) {
+      $this->attributes['created_at'] = new Carbon($date);
+  }
+
+public  function setUpdatedAtAttribute($date) {
+      $this->attributes['updated_at'] = new Carbon($date);
+  }
+}
