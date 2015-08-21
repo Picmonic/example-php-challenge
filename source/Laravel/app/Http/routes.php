54a3b2ba66884bd35eaf2694da94e7feb70f7b9f
vagrant <vagrant@homestead>
2015-08-21 09:08:05 +0000

    define routes

--- source/Laravel/app/Http/routes.php
+++ source/Laravel/app/Http/routes.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contact', 'PagesController@contact');
+Route::get('github', 'GithubController@index');
+Route::get('commits', 'GithubController@commits');
