68b539c668cc6d35385ea82ebf27fcce9ca29438
Scott Geithman <code@scottsdesigns.com>
2015-09-09 01:43:50 -0700

    removed username and password encryption setup code
    Signed-off-by: Scott Geithman <code@scottsdesigns.com>

--- themes/default/views/site/index.php
+++ themes/default/views/site/index.php
@@ -1,10 +1,5 @@
 
         <div class="row">
-            <?php
-			$password = 'admin_911';
-			$_dbsalt = 'D*eRDBSy5Pepk=3c5V$^bjp^nM6yCAcCGNY%H*k9SXvurW&pBs-gANJ+ZCscD9s!';
-			echo mb_strimwidth(hash("sha512", hash("sha512", hash("whirlpool", md5($password))) . hash("sha512", md5($password . md5($_dbsalt))) . $_dbsalt), 0, 64);
-			?>
             <div class="col-xs-12">
                 <div class="jumbotron">
                     <h1>PicMonic Commits App</h1>
