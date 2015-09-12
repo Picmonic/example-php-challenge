cb4e623133f4fd5b0ddc29d14af32165627978b6
Dommert <DommertEnterprises@Gmail.com>
2015-09-12 05:51:57 -0700

    html templates

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='example-php-challenge',
+    version='v0.0.1-beta',
+    packages=[''],
+    url='',
+    license='',
+    author='Dommert',
+    author_email='Dommert@Gmail.com',
+    description=''
+)
