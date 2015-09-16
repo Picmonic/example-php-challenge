10b4f6994da14cbdcde9bfe9c5e16a1296574290
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2015-09-15 22:05:41 -0700

    Added navbar for easy navigation

--- source/resources/views/commits.php
+++ source/resources/views/commits.php
@@ -23,6 +23,27 @@
 
 <body>
 
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/">example-php-challenge</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="/">Latest Commits</a>
+                    </li>
+                    <li>
+                        <a href="/by-author">Commits by Author</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
     <div class="container">
 
         <table class="table">
