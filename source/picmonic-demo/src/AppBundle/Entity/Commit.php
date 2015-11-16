42911f4c3e9342aa7ce54ffa22fe542a1ab268cc
PzalDigital <pete@pzaldigital.com>
2015-11-15 21:48:24 -0700

    Defined Commit Entity ORM file, used doctrine commands to create entity, database & table

---
+++ source/picmonic-demo/src/AppBundle/Entity/Commit.php
@@ -0,0 +1,398 @@
+<?php // src/AppBundle/Entity/Commit.php
+namespace AppBundle\Entity;
+
+class Commit {
+
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $sha;
+
+    /**
+     * @var string
+     */
+    private $author_name;
+
+    /**
+     * @var string
+     */
+    private $author_email;
+
+    /**
+     * @var integer
+     */
+    private $author_id;
+
+    /**
+     * @var string
+     */
+    private $committer_name;
+
+    /**
+     * @var string
+     */
+    private $committer_email;
+
+    /**
+     * @var integer
+     */
+    private $committer_id;
+
+    /**
+     * @var string
+     */
+    private $tree_sha;
+
+    /**
+     * @var string
+     */
+    private $tree_url;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $url;
+
+    /**
+     * @var string
+     */
+    private $html_url;
+
+    /**
+     * @var integer
+     */
+    private $comments;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set sha
+     *
+     * @param string $sha
+     *
+     * @return Commit
+     */
+    public function setSha($sha)
+    {
+        $this->sha = $sha;
+
+        return $this;
+    }
+
+    /**
+     * Get sha
+     *
+     * @return string
+     */
+    public function getSha()
+    {
+        return $this->sha;
+    }
+
+    /**
+     * Set authorName
+     *
+     * @param string $authorName
+     *
+     * @return Commit
+     */
+    public function setAuthorName($authorName)
+    {
+        $this->author_name = $authorName;
+
+        return $this;
+    }
+
+    /**
+     * Get authorName
+     *
+     * @return string
+     */
+    public function getAuthorName()
+    {
+        return $this->author_name;
+    }
+
+    /**
+     * Set authorEmail
+     *
+     * @param string $authorEmail
+     *
+     * @return Commit
+     */
+    public function setAuthorEmail($authorEmail)
+    {
+        $this->author_email = $authorEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get authorEmail
+     *
+     * @return string
+     */
+    public function getAuthorEmail()
+    {
+        return $this->author_email;
+    }
+
+    /**
+     * Set authorId
+     *
+     * @param integer $authorId
+     *
+     * @return Commit
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->author_id = $authorId;
+
+        return $this;
+    }
+
+    /**
+     * Get authorId
+     *
+     * @return integer
+     */
+    public function getAuthorId()
+    {
+        return $this->author_id;
+    }
+
+    /**
+     * Set committerName
+     *
+     * @param string $committerName
+     *
+     * @return Commit
+     */
+    public function setCommitterName($committerName)
+    {
+        $this->committer_name = $committerName;
+
+        return $this;
+    }
+
+    /**
+     * Get committerName
+     *
+     * @return string
+     */
+    public function getCommitterName()
+    {
+        return $this->committer_name;
+    }
+
+    /**
+     * Set committerEmail
+     *
+     * @param string $committerEmail
+     *
+     * @return Commit
+     */
+    public function setCommitterEmail($committerEmail)
+    {
+        $this->committer_email = $committerEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get committerEmail
+     *
+     * @return string
+     */
+    public function getCommitterEmail()
+    {
+        return $this->committer_email;
+    }
+
+    /**
+     * Set committerId
+     *
+     * @param integer $committerId
+     *
+     * @return Commit
+     */
+    public function setCommitterId($committerId)
+    {
+        $this->committer_id = $committerId;
+
+        return $this;
+    }
+
+    /**
+     * Get committerId
+     *
+     * @return integer
+     */
+    public function getCommitterId()
+    {
+        return $this->committer_id;
+    }
+
+    /**
+     * Set treeSha
+     *
+     * @param string $treeSha
+     *
+     * @return Commit
+     */
+    public function setTreeSha($treeSha)
+    {
+        $this->tree_sha = $treeSha;
+
+        return $this;
+    }
+
+    /**
+     * Get treeSha
+     *
+     * @return string
+     */
+    public function getTreeSha()
+    {
+        return $this->tree_sha;
+    }
+
+    /**
+     * Set treeUrl
+     *
+     * @param string $treeUrl
+     *
+     * @return Commit
+     */
+    public function setTreeUrl($treeUrl)
+    {
+        $this->tree_url = $treeUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get treeUrl
+     *
+     * @return string
+     */
+    public function getTreeUrl()
+    {
+        return $this->tree_url;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Commit
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Commit
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set htmlUrl
+     *
+     * @param string $htmlUrl
+     *
+     * @return Commit
+     */
+    public function setHtmlUrl($htmlUrl)
+    {
+        $this->html_url = $htmlUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get htmlUrl
+     *
+     * @return string
+     */
+    public function getHtmlUrl()
+    {
+        return $this->html_url;
+    }
+
+    /**
+     * Set comments
+     *
+     * @param integer $comments
+     *
+     * @return Commit
+     */
+    public function setComments($comments)
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
+
+    /**
+     * Get comments
+     *
+     * @return integer
+     */
+    public function getComments()
+    {
+        return $this->comments;
+    }
+}
