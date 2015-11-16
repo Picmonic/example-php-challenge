931fa9f65bbfa379fc148cb28ab1388a0b1e865e
PzalDigital <pete@pzaldigital.com>
2015-11-15 23:20:54 -0700

    Implemented check for pre-existing sha to prevent duplicate insertion

--- source/picmonic-demo/src/AppBundle/Models/Commits.php
+++ source/picmonic-demo/src/AppBundle/Models/Commits.php
@@ -30,28 +30,31 @@ class Commits {
 		
 		$client = new \Github\Client();
 		$commits = $client->api('repo')->commits()->all('nodejs','node', array('sha','master'));
-		
+		$repository = $this->em->getRepository('AppBundle:Commit');
+
 		for ($i = 0; $i < 25; ++$i) {
-			$commit = new ent\Commit;
 			$thisCommit = $commits[$i];
-			$commit->setSha($thisCommit['sha']);
-			$commit->setAuthorName($thisCommit['commit']['author']['name']);
-			$commit->setAuthorEmail($thisCommit['commit']['author']['email']);
-			$commit->setAuthorID($thisCommit['author']['id']);
-			$commit->setCommitterName($thisCommit['commit']['committer']['name']);
-			$commit->setCommitterEmail($thisCommit['commit']['committer']['email']);
-			$commit->setCommitterID($thisCommit['committer']['id']);
-			$commit->setTreeSha($thisCommit['commit']['tree']['sha']);
-			$commit->setTreeUrl($thisCommit['commit']['tree']['url']);
-			$commit->setMessage($thisCommit['commit']['message']);
-			$commit->setUrl($thisCommit['url']);
-			$commit->setHtmlUrl($thisCommit['html_url']);
-			$commit->setComments($thisCommit['commit']['comment_count']);
-			$commit->setCommitDate(new \DateTime($thisCommit['commit']['committer']['date']));
 
+			if(!$repository->findOneBy(array('sha' => $thisCommit['sha']))) {
+				$commit = new ent\Commit;
 
-			
-			$this->em->persist($commit);
+				$commit->setSha($thisCommit['sha']);
+				$commit->setAuthorName($thisCommit['commit']['author']['name']);
+				$commit->setAuthorEmail($thisCommit['commit']['author']['email']);
+				$commit->setAuthorID($thisCommit['author']['id']);
+				$commit->setCommitterName($thisCommit['commit']['committer']['name']);
+				$commit->setCommitterEmail($thisCommit['commit']['committer']['email']);
+				$commit->setCommitterID($thisCommit['committer']['id']);
+				$commit->setTreeSha($thisCommit['commit']['tree']['sha']);
+				$commit->setTreeUrl($thisCommit['commit']['tree']['url']);
+				$commit->setMessage($thisCommit['commit']['message']);
+				$commit->setUrl($thisCommit['url']);
+				$commit->setHtmlUrl($thisCommit['html_url']);
+				$commit->setComments($thisCommit['commit']['comment_count']);
+				$commit->setCommitDate(new \DateTime($thisCommit['commit']['committer']['date']));
+
+				$this->em->persist($commit);
+			} 
 		}
 		$this->em->flush();
         $this->em->clear(); 
