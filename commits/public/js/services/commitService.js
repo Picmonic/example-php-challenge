dcbcb13d758a59466b05d951fd92249f42976c6a
Benjamin Largent <benjamin.largent@gmail.com>
2016-01-01 17:17:43 -0700

    added commitService for angular to handle get requests

---
+++ commits/public/js/services/commitService.js
@@ -0,0 +1,19 @@
+angular.module('commitService', [])
+
+.factory('Commit', function($http) {
+    return {
+        // get all the commmits
+        get : function() {
+            return $http.get('/api/commits');
+        }
+
+        /***
+         * Could do the github api stuff here and send back via route
+         * for Laravel endpoint to deal with, but let's keep github api
+         * routines in laravel for now.
+         *
+         * save : function(commitData ... etc etc ...){},
+         *
+         */
+    }
+});
