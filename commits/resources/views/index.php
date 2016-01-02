d360e5d284bd0d4106aaab1d6dca877b9671ef9e
Benjamin Largent <benjamin.largent@gmail.com>
2016-01-01 17:44:26 -0700

    something fishy going on. no output to default view. hmmmmmm. may be time for a break

--- commits/resources/views/index.php
+++ commits/resources/views/index.php
@@ -7,7 +7,7 @@
     <style>
         body        { padding-top:30px; }
         form        { padding-bottom:20px; }
-        .comment    { padding-bottom:20px; }
+        .commit    { padding-bottom:20px; }
     </style>
 
     <!-- JS -->
@@ -22,29 +22,29 @@
 
 
 </head>
-
 <!-- declare our angular app and controller -->
-<body class="container" ng-app="commitApp" ng-controller="mainController"> <div class="col-md-8 col-md-offset-2">
+<body class="container" ng-app="commitApp" ng-controller="mainController">
+    <div class="col-md-8 col-md-offset-2">
 
-    <!-- PAGE TITLE =============================================== -->
-    <div class="page-header">
-        <h2>Laravel and Angular monitor commits for nodejs/node</h2>
-        <h4>nodejs commit monitoring</h4>
+        <!-- PAGE TITLE =============================================== -->
+        <div class="page-header">
+            <h2>Laravel and Angular monitor commits for nodejs/node</h2>
+            <h4>nodejs commit monitoring</h4>
+        </div>
+
+        <!-- LOADING ICON =============================================== -->
+        <!-- show loading icon if the loading variable is set to true -->
+        <p class="text-center" ng-show="loading"><span class="fa fa-meh-o fa-5x fa-spin"></span></p>
+
+        <!-- THE COMMITS =============================================== -->
+        <!-- hide these commits if the loading variable is true -->
+<!--        <div class="commit" ng-hide="loading" ng-repeat="commit in commits">-->
+<!--            <h3>Commit author: {{ commit.author }}</h3>-->
+<!--            <p> <small>SHA: {{ commit.sha }}</small><br>-->
+<!--                <small>Date: {{ commit.date }}</small><br>-->
+<!--                {{ commit.msg }}-->
+<!--            </p>-->
+<!--        </div>-->
     </div>
-
-    <!-- LOADING ICON =============================================== -->
-    <!-- show loading icon if the loading variable is set to true -->
-    <p class="text-center" ng-show="loading"><span class="fa fa-meh-o fa-5x fa-spin"></span></p>
-
-    <!-- THE COMMITS =============================================== -->
-    <!-- hide these commits if the loading variable is true -->
-    <div class="commit" ng-hide="loading" ng-repeat="commit in commits | orderBy:'author'">
-        <h3>Commit author:</h3>
-        <p> <small>SHA:</small><br>
-            <small>Date:</small><br>
-
-        </p>
-    </div>
-</div>
 </body>
 </html>
