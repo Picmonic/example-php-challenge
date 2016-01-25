22c873cc14f801f5d339f8a9d5adc344335c5094
root <root@myq.myqkeeper.com>
2016-01-24 22:57:18 -0600

    Added messaging for stale data

--- source/js/controllers/CommitsController.js
+++ source/js/controllers/CommitsController.js
@@ -2,15 +2,34 @@ app.controller('CommitsController', ['$scope', 'CommitsService',
 function($scope, CommitsService) {
 
   $scope.commits = {};
+  $scope.message = {
+    type : 'info',
+    message : 'Loading commits'
+  };
 
   CommitsService.getCommits().then(function(response){
     var data = [];
+    var last = '';
     for (var i = 0; i < response.data.length; i++) {
       var datum = response.data[i];
       var lastChar = parseInt(datum.hash.slice(-1));
       var highlight = isNaN(lastChar);
       datum.highlight = !highlight;
       data.push(datum);
+      last = datum.modified.split(/[- :]/);
+    }
+    last = new Date(last[0], last[1]-1, last[2], last[3], last[4], last[5]);
+    
+    if ((new Date) - last > 1000 * 60 * 10) {
+      $scope.message = {
+        type : 'warning',
+        message : 'Data more than 10 minutes old, consider flushing commits'
+      }
+    } else {
+      $scope.message = {
+        type : 'success',
+        message : 'Data last fetched ' + last
+      }
     }
     $scope.commits = data;
   });
