da708c988dfdfd168b23a6dd0f034bf89e6f6d48
Andrew P. Ewing <drew@phenocode.com>
2016-01-26 13:04:45 -0700

    Implemented React

---
+++ source/resources/assets/components/Home/home.jsx
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+
+class Home extends React.Component {
+	render() {
+		return (
+			<div className="home-container container-fluid">
+				<h2>Homepage</h2>
+				<p>Welcome to the homepage!</p>
+			</div>
+		);
+	}
+}
+
+export default Home;
