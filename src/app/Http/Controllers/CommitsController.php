57790d9c2dd7f3e7c9bc329a8fa2b8681df09e8b
Adrian Carriger <mail@adrianandnicole.com>
2017-02-03 11:25:58 -0800

    feat(api): add commits endpoint

---
+++ src/app/Http/Controllers/CommitsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CommitsController extends Controller
+{
+    public function index()
+	{
+        return 'nothing here yet';
+    }
+}
