b619d4e47d19e974f885d4dedcbb668949b8bd7e
Adrian Carriger <mail@adrianandnicole.com>
2017-02-03 12:53:50 -0800

    feat(angular): connect to api

--- src/angular/app/app.component.ts
+++ src/angular/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ApiService } from './core/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  constructor(private api: ApiService) { }
+  ngOnInit() {
+    this.api.getStuff().subscribe(stuff => console.log(stuff));
+  }
 }
--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -10,5 +10,5 @@
 <link href="styles.d41d8cd98f00b204e980.bundle.css" rel="stylesheet"/></head>
 <body>
   <app-root>Loading...</app-root>
-<script type="text/javascript" src="inline.7cefa61720330e8f3f0e.bundle.js"></script><script type="text/javascript" src="vendor.8c0c9e99e87ec5d9c3de.bundle.js"></script><script type="text/javascript" src="main.666f866fff4be4fab145.bundle.js"></script></body>
+<script type="text/javascript" src="inline.7cd3ea22deb3b42de13d.bundle.js"></script><script type="text/javascript" src="vendor.4e5ad6ec06a30150919e.bundle.js"></script><script type="text/javascript" src="main.a13f9fb8f82213caa467.bundle.js"></script></body>
 </html>
